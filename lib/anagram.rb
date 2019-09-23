4f0ec20bbef110d62aae1b4631b923fac139ecdf
Rachel T <rachmaybe@gmail.com>
2019-09-23 15:00:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,5 +10,9 @@ def match(array_of_words)
    array_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
   end
+end 
+
+listen = Anagram.new("listen")
+listen.match
 
 end 
